642d45266307de38173cf6b4b469aa2b9b67859e
Valentin <pamerra@mail.ru>
2025-08-10 09:58:44 +0300

    Initial Commit

---
+++ BestRecipes/BestRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestRecipesApp.swift
+//  BestRecipes
+//
+//  Created by Валентин on 10.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BestRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
