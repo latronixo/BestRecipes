78f6060a600a51346be6443b3d98d20f98a5fc41
Artem Tarasov <artemdrolllted@gmail.com>
2025-08-10 23:03:51 +0300

    Step 2 - add customLines

--- BestRecipes/CustomTabBar/New Shape/RectangleTopShape.swift
+++ BestRecipes/CustomTabBar/New Shape/RectangleTopShape.swift
@@ -5,4 +5,24 @@
 //  Created by Drolllted on 10.08.2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct RectangleTopShape: Shape {
+    
+    let radius: CGFloat
+    
+    nonisolated func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: radius))
+        path.addArc(center: CGPoint(x: radius, y: radius), radius: radius, startAngle: .degrees(180), endAngle: .degrees(270), clockwise: false)
+        path.addLine(to: CGPoint(x: rect.width - radius, y: 0))
+        path.addArc(center: CGPoint(x: rect.width - radius, y: radius), radius: radius, startAngle: .degrees(270), endAngle: .degrees(0), clockwise: false)
+        path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+        path.addLine(to: CGPoint(x: 0, y: rect.height))
+        path.closeSubpath()
+        
+        return path
+    }
+    
+}
