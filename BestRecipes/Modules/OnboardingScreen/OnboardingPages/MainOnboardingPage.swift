ad83cba85c858fe1ab2e4f5cb1b9f1d6951a889c
Artem Tarasov <artemdrolllted@gmail.com>
2025-08-11 21:55:06 +0300

    Step 1 - setup first screen in onboarding

---
+++ BestRecipes/Modules/OnboardingScreen/OnboardingPages/MainOnboardingPage.swift
@@ -0,0 +1,18 @@
+//
+//  MainOnboardingPage.swift
+//  BestRecipes
+//
+//  Created by Drolllted on 11.08.2025.
+//
+
+import SwiftUI
+
+struct MainOnboardingPage: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview{
+    MainOnboardingPage()
+}
---
+++ BestRecipes/Modules/OnboardingScreen/OnboardingView.swift
@@ -0,0 +1,77 @@
+//
+//  OnboardingView.swift
+//  BestRecipes
+//
+//  Created by Drolllted on 11.08.2025.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        NavigationStack{
+            ZStack{
+                Image("backOnboardingImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+                
+                LinearGradient(colors: [.clear, .black], startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea(.all)
+                
+                VStack(alignment: .center, spacing: 40) {
+                    HStack(alignment: .center, spacing: 4){
+                        Image(systemName: "star.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundStyle(.black)
+                        
+                        Text("100k+ Premium Recipes")
+                            .font(.system(size: 18))
+                            .foregroundStyle(.white)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .center, spacing: 20) {
+                        VStack(alignment: .center){
+                            Text("Best")
+                            
+                            Text("Recipes")
+                        }
+                        .font(.system(size: 56))
+                        .foregroundStyle(.white)
+                        .bold()
+                        
+                        Text("Find best recipes for cooking")
+                            .font(.system(size: 16))
+                            .foregroundStyle(.white)
+                    }
+                    
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Continue")
+                            .font(.system(size: 18))
+                            .foregroundStyle(Color.white)
+                            .bold()
+                            .background {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(Color.red.opacity(0.6))
+                                    .frame(width: 300, height: 50)
+                            }
+                    }
+                    .padding()
+                    
+                    
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview{
+    OnboardingView()
+}
