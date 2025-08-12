ccd19b156f9ea3b3e178b7748b86ba9823c16587
Artem Tarasov <artemdrolllted@gmail.com>
2025-08-13 01:02:45 +0300

    Step 3 - bug fix and work with TabView

--- BestRecipes/Modules/OnboardingScreen/OnboardingPages/ContsructOnboardingView.swift
+++ BestRecipes/Modules/OnboardingScreen/OnboardingPages/ContsructOnboardingView.swift
@@ -10,68 +10,73 @@ import SwiftUI
 struct ContsructOnboardingView: View {
     
     var enumConstraction: EnumConstructor
-    let whiteText: String
-    let greenText: String
     let thirdScreen: Bool
+    @Binding var currentPage: Int
+    @Binding var totalPages: Int
     
     var body: some View {
         ZStack{
-            Image("\(enumConstraction.backgroundImage)")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .ignoresSafeArea()
+            GeometryReader { geometry in
+                Image("\(enumConstraction.backgroundImage)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .clipped()
+            }
+            .ignoresSafeArea()
             
             LinearGradient(colors: [.clear, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
-            VStack(alignment: .center, spacing: 30) {
+            VStack(alignment: .center, spacing: 70) {
                 Spacer()
-                
-                switch enumConstraction {
-                case .first:
-                    VStack(spacing: 8) {
-                        Text("Recipes from")
-                            .foregroundStyle(.white)
-                        HStack {
-                            Text("all")
+                Group{
+                    switch enumConstraction {
+                    case .first:
+                        VStack(spacing: 8) {
+                            Text("Recipes from")
                                 .foregroundStyle(.white)
+                            HStack {
+                                Text("all")
+                                    .foregroundStyle(.white)
+                                
+                                Text("over the")
+                                    .foregroundStyle(.green)
+                            }
                             
-                            Text("over the")
+                            Text("World")
+                                .foregroundStyle(.green)
+                            
+                        }
+                        
+                    case .second:
+                        VStack(spacing: 8){
+                            Text("Recipes with")
+                                .foregroundStyle(.white)
+                            Text("each and every")
+                                .foregroundStyle(.green)
+                            Text("detail")
                                 .foregroundStyle(.green)
                         }
                         
-                        Text("World")
-                            .foregroundStyle(.green)
-                        
+                    case .third:
+                        VStack(spacing: 8) {
+                            Text("Cook it now or")
+                                .foregroundStyle(.white)
+                            Text("save it for later")
+                                .foregroundStyle(.green)
+                        }
                     }
-                    .font(.system(size: 40))
-                    .fontWeight(.semibold)
-                    
-                case .second:
-                    VStack(spacing: 8){
-                        Text("Recipes with")
-                            .foregroundStyle(.white)
-                        Text("each and every detail")
-                            .foregroundStyle(.green)
-                    }
-                    .font(.system(size: 40))
-                    .fontWeight(.semibold)
-                    
-                case .third:
-                    VStack(spacing: 8) {
-                        Text("Cook it now or")
-                            .foregroundStyle(.white)
-                        Text("save it for later")
-                            .foregroundStyle(.green)
-                    }
-                    .font(.system(size: 40))
-                    .fontWeight(.semibold)
                 }
+                .font(.system(size: 40))
+                .fontWeight(.semibold)
                 
                 VStack(alignment: .center, spacing: 8){
                 
                 Button {
-                    
+                    if currentPage < totalPages - 1{
+                        currentPage += 1
+                    }
                 } label: {
                     Text(thirdScreen ? "Start Cooking" : "Continue")
                     .font(.system(size: 18))
@@ -87,7 +92,7 @@ struct ContsructOnboardingView: View {
                 
                     if thirdScreen == false {
                         Button {
-                            
+                            currentPage = totalPages - 1
                         } label: {
                             Text("Skip")
                                 .fontWeight(.light)
@@ -104,8 +109,4 @@ struct ContsructOnboardingView: View {
             
         }
     }
-}
-
-#Preview {
-    ContsructOnboardingView(enumConstraction: .first, whiteText: "Recipes for all", greenText: "over the World", thirdScreen: false)
 }
--- BestRecipes/Modules/OnboardingScreen/OnboardingPages/MainOnboardingPage.swift
+++ BestRecipes/Modules/OnboardingScreen/OnboardingPages/MainOnboardingPage.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct MainOnboardingPage: View {
+    
+    @State private var currentPage: Int = 0
+    @State private var totalPages = 3
+    
     var body: some View {
-        TabView{
-            
+        ZStack(alignment: .bottom){
+            TabView(selection: $currentPage){
+                ContsructOnboardingView(enumConstraction: .first, thirdScreen: false, currentPage: $currentPage, totalPages: $totalPages)
+                    .tag(0)
+                ContsructOnboardingView(enumConstraction: .second, thirdScreen: false, currentPage: $currentPage, totalPages: $totalPages)
+                    .tag(1)
+                ContsructOnboardingView(enumConstraction: .third, thirdScreen: true, currentPage: $currentPage, totalPages: $totalPages)
+                    .tag(2)
+            }
+            .ignoresSafeArea(.all)
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            HStack(spacing: 8) {
+                ForEach(0..<totalPages, id: \.self) { index in
+                    Capsule()
+                        .fill(index == currentPage ? Color.green : Color.gray.opacity(0.5))
+                        .frame(width: 40, height: 8)
+                        .animation(.easeInOut, value: currentPage)
+                }
+            }
+            .padding(.bottom, currentPage == totalPages - 1 ? 100 : 120)
         }
-        .tabViewStyle(.page)
     }
 }
 
