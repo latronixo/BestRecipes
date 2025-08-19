5da24cdf2faf3f02a687483d2e3b4bd4847dd803
Valentin <pamerra@mail.ru>
2025-08-19 21:47:24 +0300

    Перенес навигацию из CustomTabBar в MainView. Добавил переходы с HomeScreen на DetailRecipe и See All

--- BestRecipes/Modules/MainView/MainView.swift
+++ BestRecipes/Modules/MainView/MainView.swift
@@ -12,31 +12,51 @@ import SwiftUI
 struct MainView: View {
     
     @StateObject private var router = Router()
+    @State private var selectedTab: TabEnum = .home
     
     var body: some View {
         
         NavigationStack(path: $router.path) {
-            CustomTabBar()
-                    .navigationDestination(for: Routes.self) { route in
-                        switch route {
-                        case .homeScreen:
-                            ContentView()
-                        case .detailScreen(recipeDetails: let recipeDetails):
-                            DetailView(detailVM: DetailViewModel(recipe: recipeDetails, router: router))
-                        case .seeAllScreen(let category):
-                            SeeAllView(category: category)
-                        case .searchScreen:
-                            SearchView()
-                        case .createScreen:
-                            AddRecipeView()
-                        case .profileScreen:
-                            PersonView()
-                        }
-                        
-                    }
+            ZStack(alignment: .bottom) {
+                TabView(selection: $selectedTab) {
+                    HomeScreenView()
+                        .tag(TabEnum.home)
+                    
+                    DiscoverView(favorites: Array(repeating: .preview, count: 3))
+                        .tag(TabEnum.bookmarks)
+                    
+                    AddRecipeView()
+                        .tag(TabEnum.add)
+                    
+                    BellView()
+                        .tag(TabEnum.notifications)
+                    
+                    PersonView()
+                        .tag(TabEnum.profile)
+                }
+                
+                CustomTabBar(selectedTab: $selectedTab)
             }
-            .environmentObject(router)
-        
+            .ignoresSafeArea(.keyboard)
+            .navigationDestination(for: Routes.self) { route in
+                switch route {
+                case .homeScreen:
+                    HomeScreenView()
+                case .detailScreen(recipeDetails: let recipeDetails):
+                    DetailView(detailVM: DetailViewModel(recipe: recipeDetails, router: router))
+                case .seeAllScreen(let category):
+                    SeeAllView(category: category)
+                case .searchScreen:
+                    SearchView()
+                case .createScreen:
+                    AddRecipeView()
+                case .profileScreen:
+                    PersonView()
+                }
+                
+            }
+        }
+        .environmentObject(router)
     }
 }
 
