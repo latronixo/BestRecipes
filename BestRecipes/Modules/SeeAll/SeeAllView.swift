2685fe6f25ca32e97f5a4780c3ecbf5e2a1a765e
ssgushyonka <orangeee04@gmail.com>
2025-08-21 10:51:03 +0300

    add seeAll view

--- BestRecipes/Modules/SeeAll/SeeAllView.swift
+++ BestRecipes/Modules/SeeAll/SeeAllView.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct SeeAllView: View {
+    @EnvironmentObject var router: Router
     @StateObject private var viewModel = SeeAllViewModel()
     let category: SeeAllCategory
+    @State private var recipes: [Recipe] = []
+    private let tabBarHeight: CGFloat = 60
     
     var body: some View {
         ScrollView {
-            Text("Отображение для категории: \(category.title)")
-                .font(.title)
-                .padding()
-            
+            VStack(spacing: 24) {
+                ForEach(viewModel.trendingRecipes) { recipe in
+                    Button {
+                        router.goTo(to: .detailScreen(recipeId: recipe.id))
+                    } label: {
+                        RecipeCardView(recipe: recipe)
+                    }
+                }
+            }
+            .padding(.vertical, 16)
+            .padding(.bottom, tabBarHeight)
         }
         .navigationTitle(category.title)
         .navigationBarTitleDisplayMode(.inline)
