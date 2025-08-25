ef470897d2ea206b2ed6818d56dda9a88a7db463
Artem Tarasov <artemdrolllted@gmail.com>
2025-08-25 14:22:06 +0300

    Step 2 - write launchScreen and add LottieAnimation

--- BestRecipes/Modules/LaunchScreen/AnimationLaunchScreen.swift
+++ BestRecipes/Modules/LaunchScreen/AnimationLaunchScreen.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct AnimationLaunchScreen: View {
     
@@ -22,7 +23,31 @@ struct AnimationLaunchScreen: View {
                 .ignoresSafeArea(.all)
             
             VStack(alignment: .center, spacing: 30) {
+                LottieView(animation: .named("cooking-animation"))
+                    .playbackMode(.playing(.toProgress(1, loopMode: .playOnce)))
+                    .frame(width: 200, height: 200)
                 
+                if showText {
+                    VStack(spacing: 5){
+                        Text("Best Recipes")
+                            .font(.poppinsSemibold(size: 40))
+                            .foregroundStyle(.white)
+                            .transition(.scale.combined(with: .opacity))
+                        
+                        Text("100k+ Premium Recipes")
+                            .font(.poppinsRegular(size: 16))
+                            .foregroundColor(.white)
+                            .padding(.top, 10)
+                            .transition(.scale.combined(with: .opacity))
+                    }
+                }
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
+                    showText = true
+                }
             }
         }
     }
